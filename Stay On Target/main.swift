dd828fa120739e0fc9a5b85f31c632e928f00cf1
Adam Israel <adam@adamisrael.com>
2014-06-19 19:17:51 -0400

    Skeleton project
    Signed-off-by: Adam Israel <adam@adamisrael.com>

---
+++ Stay On Target/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Stay On Target
+//
+//  Created by Adam @ Work on 2014-06-19.
+//  Copyright (c) 2014 Adam Israel. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
